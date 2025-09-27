bb6afb9366d9f73774051c8e558832b5aa7f41a2
Thomas Sutton <tommy.sutton@live.com>
2025-09-27 17:24:37 +0100

    styling improvments

--- dora/persona/basic.py
+++ dora/persona/basic.py
@@ -1,5 +1,6 @@
 
 from dora.tools.numbers import NumberTool
+from dora.tools.web_scrape import WebscrapeTool
 from openai import OpenAI
 from typing import List, Dict
 import logging
@@ -7,27 +8,29 @@ import json
 
 GPT_MODEL = "gpt-3.5-turbo"
 TOOLS = [
-    NumberTool
-] 
+    NumberTool,
+    WebscrapeTool
+]
+
 
 class BasicPersona:
 
     def __init__(self, name: str, description: str) -> None:
-       self.log = logging.getLogger(__name__)
-       self.name = name
-       self.role = "user"
-       self.description = description
-       self.tools = self._load_tools()
-       self.c_openai = OpenAI()
+        self.log = logging.getLogger(__name__)
+        self.name = name
+        self.role = "user"
+        self.description = description
+        self.tools = self._load_tools()
+        self.c_openai = OpenAI()
 
     def __str__(self) -> str:
         return f"{self.name}: {self.description}"
-    
-    def _load_tools(self) -> list: 
+
+    def _load_tools(self) -> list:
         return [x() for x in TOOLS]
-    
-    def _create_response(self, input_list: list[Dict]=None) -> Dict:
-        
+
+    def _create_response(self, input_list: list[Dict]) -> Dict:
+
         return self.c_openai.responses.create(
             model=GPT_MODEL,
             tools=[
@@ -35,7 +38,7 @@ class BasicPersona:
             ],
             input=input_list
         )
-    
+
     def _context(self) -> List[Dict]:
         return [{"role": "system", "content": self.description}]
 
@@ -47,24 +50,27 @@ class BasicPersona:
 
         # Process any calls to tools
         for item in resp.output:
-            
+
             if item.type == "function_call":
 
                 # Get the tool function from the tools list
                 try:
-                    func = next((t for t in self.tools if t.name == item.name), None)
+                    func = next(
+                        (t for t in self.tools if t.name == item.name),
+                        None
+                    )
                 except AttributeError as e:
                     self.log.error(f"Function {item.name} not found: {e}")
                     raise e
-                
+
                 # Call the tool function with the provided arguments
                 args = json.loads(item.arguments)
                 try:
                     tool_response = func(**args)
                 except TypeError as e:
-                    self.log.error(f"Error calling function {item.name} with arguments {args}: {e}")
+                    self.log.error(f"Err calling func {item.name} {args}: {e}")
                     raise e
-                
+
                 input_list.append({
                     "type": "function_call_output",
                     "call_id": item.call_id,
@@ -74,5 +80,5 @@ class BasicPersona:
         self.log.debug("Generated function calls:")
         for line in input_list:
             self.log.debug(f"\t{line}")
-        
+
         return self._create_response(input_list)
