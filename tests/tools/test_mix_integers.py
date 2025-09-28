2c0da6de1c4215635cbc927ceefa39f568e5ec05
Thomas Sutton <tommy.sutton@live.com>
2025-09-28 15:45:42 +0100

    cleanup mix integer test code

--- tests/tools/test_mix_integers.py
+++ tests/tools/test_mix_integers.py
@@ -1,12 +1,12 @@
+from dora.persona.basic import get_text_from_response
 from tests import logger
 
 
 def test_basic_maths(persona):
-    
+
     prompt = """
         Mix some random numbers only using tools. show me the numbers you are mixing
         and then give me the results in a final answer.
     """
     response = persona.request_w_tools(prompt)
-    logger.debug(f"Response: {response.model_dump_json(indent=2)}")
-    logger.info(f"Response: {response.output[-1].content[-1].text}")
+    logger.info(get_text_from_response(response))
